a95d8669c0f38a4e5cc39645bc54e31c3ba5014a
taufan <locust.ade4@gmail.com>
2021-12-20 18:06:47 +0700

    add sample data

--- detik_transaction.sql
+++ detik_transaction.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 20, 2021 at 10:55 AM
+-- Generation Time: Dec 20, 2021 at 11:05 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -41,6 +41,17 @@ CREATE TABLE `transaction_table` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `transaction_table`
+--
+
+INSERT INTO `transaction_table` (`id`, `invoice_id`, `item_name`, `amount`, `payment_type`, `customer_name`, `merchant_id`, `references_id`, `number_va`, `status`) VALUES
+(46, 1, 'mobil', 10000, 'credit_card', 'taufan', '12345', '27514116562223154', NULL, 'Failed'),
+(47, 2, 'motor', 20000, 'virtual_account', 'taufan', '54321', '27514116562223155', '27514116562223156', 'Paid'),
+(48, 3, 'Gelas', 2000, 'virtual_account', 'taufan', '3123123', '27514116562223157', '27514116562223158', 'Pending'),
+(49, 4, 'Piring', 2000, 'credit_card', 'gofur', '3123123', '27514116562223159', NULL, 'Pending'),
+(50, 5, 'laptop', 200000, 'virtual_account', 'adit', '31235542', '27514116562223160', '27514116562223161', 'Pending');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -59,7 +70,7 @@ ALTER TABLE `transaction_table`
 -- AUTO_INCREMENT for table `transaction_table`
 --
 ALTER TABLE `transaction_table`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
